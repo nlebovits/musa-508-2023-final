23ac7a48238482061f2aa631302b816c7b0d57a5
Laura Frances <laurafrancesemail@gmail.com>
2023-12-03 10:53:09 -0500

    2014 and 2019
    shall I merge phl14 and phl19 into one acs_phl?

--- wrangle_acs_data.R
+++ wrangle_acs_data.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(tidyverse)
 library(acs)
 library(tidycensus)
@@ -20,7 +21,7 @@ select <- dplyr::select
 # overcrowindg
 
 
-phl_acs <- get_acs(geography = "tract", 
+phl_acs14 <- get_acs(geography = "tract", 
                      variables = c("B01003_001", #totalpop
                                    "B19013_001", #medinc
                                    "B02001_002", #white_pop
@@ -28,7 +29,8 @@ phl_acs <- get_acs(geography = "tract",
                                    "B08012_001", #num_commuters
                                    "B08301_001", #means_of_trans
                                    "B08301_010", #total_public_trans
-                                   "B25003_003", #renter occupied
+                                   "B11012_001", #totalhh
+                                   "B25003_003", #renter_occupied
                                    "B25064_001", #medgross_rent
                                    "B25070_002", #less_10p
                                    "B25070_003", #10to15
@@ -38,7 +40,7 @@ phl_acs <- get_acs(geography = "tract",
                                    "B25070_007", #30to35
                                    "B25070_008", #35to40
                                    "B25070_009", #40to50
-                                   "B25070_010", #50+
+                                   "B25070_010" #50+
                                    ), 
                      year = 2014, 
                      state = "PA", 
@@ -52,10 +54,12 @@ phl_acs <- get_acs(geography = "tract",
          Num_Commuters = B08012_001E,
          Means_of_Transport = B08301_001E,
          Total_Public_Trans = B08301_010E,
+         Total_HH = B11012_001E,
          Renters = B25003_003E,
          Med_Gross_Rent = B25064_001E,
          Less_10 = B25070_002E,
          Less_15 = B25070_003E,
+         Less_20 = B25070_004E,
          Less_25 = B25070_005E, 
          Less_30 = B25070_006E,
          Less_35 = B25070_007E,
@@ -63,21 +67,20 @@ phl_acs <- get_acs(geography = "tract",
          Less_50 = B25070_009E, 
          More_50 = B25070_010E) %>%
   select(Total_Pop, Med_Inc, White_Pop, Travel_Time,
-         Means_of_Transport, Total_Public_Trans, Renters, Med_Gross_Rent, Less_10, Less_15,
+         Means_of_Transport, Total_Public_Trans, Total_HH, Renters, Med_Gross_Rent, Less_10, Less_15, Less_20,
          Less_25, Less_30, Less_35, Less_40, Less_50, More_50,
          GEOID, geometry) %>%
   mutate(Percent_White = White_Pop / Total_Pop,
          Percent_Nonwhite = 1-Percent_White, 
          Mean_Commute_Time = Travel_Time / Total_Public_Trans,
          Percent_Taking_Public_Trans = Total_Public_Trans / Means_of_Transport,
-         Percent_Renters = Renters / Total_Pop,
+         Percent_Renters = Renters / Total_HH,
          Rent_Burden = (Less_30 + Less_35) / Renters,
          Ext_Rent_Burden = (Less_40 + Less_50 + More_50) / Renters,
          No_Rent_Burden = (Less_10 + Less_15 + Less_20 + Less_25) / Renters) %>%
   mutate(
     nb = st_knn(geometry, 5), # need to impute census values for tracts with pop ~ 0
     wt = st_weights(nb),
-    Med_Age = ifelse(is.na(Med_Age), purrr::map_dbl(find_xj(Med_Age, nb), mean, na.rm = TRUE), Med_Age),
     Med_Inc = ifelse(is.na(Med_Inc), purrr::map_dbl(find_xj(Med_Inc, nb), mean, na.rm = TRUE), Med_Inc),
     Travel_Time = ifelse(is.na(Travel_Time), purrr::map_dbl(find_xj(Travel_Time, nb), mean, na.rm = TRUE), Travel_Time),
     Percent_White = ifelse(is.na(Percent_White), purrr::map_dbl(find_xj(Percent_White, nb), mean, na.rm = TRUE), Percent_White),
@@ -88,8 +91,7 @@ phl_acs <- get_acs(geography = "tract",
     Ext_Rent_Burden = ifelse(is.na(Ext_Rent_Burden), purrr::map_dbl(find_xj(Ext_Rent_Burden, nb), mean, na.rm = TRUE), Ext_Rent_Burden))
 
 
-
-phl_acs <- get_acs(geography = "tract", 
+phl_acs19 <- get_acs(geography = "tract", 
                    variables = c("B01003_001", #totalpop
                                  "B19013_001", #medinc
                                  "B02001_002", #white_pop
@@ -97,7 +99,8 @@ phl_acs <- get_acs(geography = "tract",
                                  "B08012_001", #num_commuters
                                  "B08301_001", #means_of_trans
                                  "B08301_010", #total_public_trans
-                                 "B25003_003", #renter occupied
+                                 "B11012_001", #totalhh
+                                 "B25003_003", #renter_occupied
                                  "B25064_001", #medgross_rent
                                  "B25070_002", #less_10p
                                  "B25070_003", #10to15
@@ -107,7 +110,7 @@ phl_acs <- get_acs(geography = "tract",
                                  "B25070_007", #30to35
                                  "B25070_008", #35to40
                                  "B25070_009", #40to50
-                                 "B25070_010", #50+
+                                 "B25070_010" #50+
                    ), 
                    year = 2019, 
                    state = "PA", 
@@ -121,10 +124,12 @@ phl_acs <- get_acs(geography = "tract",
          Num_Commuters = B08012_001E,
          Means_of_Transport = B08301_001E,
          Total_Public_Trans = B08301_010E,
+         Total_HH = B11012_001E,
          Renters = B25003_003E,
          Med_Gross_Rent = B25064_001E,
          Less_10 = B25070_002E,
          Less_15 = B25070_003E,
+         Less_20 = B25070_004E,
          Less_25 = B25070_005E, 
          Less_30 = B25070_006E,
          Less_35 = B25070_007E,
@@ -132,21 +137,20 @@ phl_acs <- get_acs(geography = "tract",
          Less_50 = B25070_009E, 
          More_50 = B25070_010E) %>%
   select(Total_Pop, Med_Inc, White_Pop, Travel_Time,
-         Means_of_Transport, Total_Public_Trans, Renters, Med_Gross_Rent, Less_10, Less_15,
+         Means_of_Transport, Total_Public_Trans, Total_HH, Renters, Med_Gross_Rent, Less_10, Less_15, Less_20,
          Less_25, Less_30, Less_35, Less_40, Less_50, More_50,
          GEOID, geometry) %>%
   mutate(Percent_White = White_Pop / Total_Pop,
          Percent_Nonwhite = 1-Percent_White, 
          Mean_Commute_Time = Travel_Time / Total_Public_Trans,
          Percent_Taking_Public_Trans = Total_Public_Trans / Means_of_Transport,
-         Percent_Renters = Renters / Total_Pop,
+         Percent_Renters = Renters / Total_HH,
          Rent_Burden = (Less_30 + Less_35) / Renters,
          Ext_Rent_Burden = (Less_40 + Less_50 + More_50) / Renters,
          No_Rent_Burden = (Less_10 + Less_15 + Less_20 + Less_25) / Renters) %>%
   mutate(
     nb = st_knn(geometry, 5), # need to impute census values for tracts with pop ~ 0
     wt = st_weights(nb),
-    Med_Age = ifelse(is.na(Med_Age), purrr::map_dbl(find_xj(Med_Age, nb), mean, na.rm = TRUE), Med_Age),
     Med_Inc = ifelse(is.na(Med_Inc), purrr::map_dbl(find_xj(Med_Inc, nb), mean, na.rm = TRUE), Med_Inc),
     Travel_Time = ifelse(is.na(Travel_Time), purrr::map_dbl(find_xj(Travel_Time, nb), mean, na.rm = TRUE), Travel_Time),
     Percent_White = ifelse(is.na(Percent_White), purrr::map_dbl(find_xj(Percent_White, nb), mean, na.rm = TRUE), Percent_White),
@@ -155,5 +159,6 @@ phl_acs <- get_acs(geography = "tract",
     Percent_Renters =  ifelse(is.na(Percent_Renters), purrr::map_dbl(find_xj(Percent_Renters, nb), mean, na.rm = TRUE), Percent_Renters),
     Rent_Burden = ifelse(is.na(Rent_Burden), purrr::map_dbl(find_xj(Rent_Burden, nb), mean, na.rm = TRUE), Rent_Burden),
     Ext_Rent_Burden = ifelse(is.na(Ext_Rent_Burden), purrr::map_dbl(find_xj(Ext_Rent_Burden, nb), mean, na.rm = TRUE), Ext_Rent_Burden))
-  
-saveRDS(acs_vars, acs_vars_path) # path already defined in config file
+
+
+saveRDS(phl_acs, acs_vars_path) # path already defined in config file
