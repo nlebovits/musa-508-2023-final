8b91f39d5a2f0fcc4730cd24067b0d0b37e4f5e2
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2023-12-14 11:16:46 -0500

    update app dataset w council dists, area

--- utils/wrangle_app_data.R
+++ utils/wrangle_app_data.R
@@ -9,7 +9,8 @@ filter <- dplyr::filter
 # load data---------------------------------------------------
 urls <- c(
   zoning = "https://opendata.arcgis.com/datasets/0bdb0b5f13774c03abf8dc2f1aa01693_0.geojson",
-  rf_val_preds = './data/model_outputs/rf_val_preds.geojson'  # Updated file path
+  rf_val_preds = './data/model_outputs/rf_val_preds.geojson',  # Updated file path
+  council_dists = "https://opendata.arcgis.com/datasets/9298c2f3fa3241fbb176ff1e84d33360_0.geojson"
 )
 
 suppressMessages({
@@ -18,6 +19,8 @@ suppressMessages({
   )
 })
 
+council_dists <- council_dists %>%
+  select(DISTRICT)
 
 # data wrangle-----------------------------------------------
 filtered_zoning <- zoning %>%
@@ -26,12 +29,13 @@ filtered_zoning <- zoning %>%
          !str_detect(CODE, "SP")) %>%
   st_join(., rf_val_preds %>% select(rf_val_preds))
 
+
 nbs <- filtered_zoning %>% 
   mutate(nb = st_contiguity(geometry))
 
 # Create edge list while handling cases with no neighbors
 edge_list <- tibble::tibble(id = 1:length(nbs$nb), nbs = nbs$nb) %>% 
-  tidyr::unnest(nbs) %>% 
+  unnest(nbs) %>% 
   filter(nbs != 0)
 
 # Create a graph with a node for each row in filtered_zoning
@@ -44,18 +48,24 @@ if (nrow(edge_list) > 0) {
   g <- add_edges(g, c(t(edges)))
 }
 
-# Calculate the number of contiguous neighbors, handling nodes without neighbors
-n_contiguous <- sapply(V(g)$name, function(node) {
-  if (node %in% edges) {
-    length(neighborhood(g, order = 1, nodes = as.numeric(node))[[1]])
-  } else {
-    1  # Nodes without neighbors count as 1 (themselves)
-  }
-})
+# Calculate the number of contiguous neighbors and sum of contiguous areas
+n_contiguous <- numeric(nrow(filtered_zoning))
+sum_contig_area <- numeric(nrow(filtered_zoning))
 
-filtered_zoning <- filtered_zoning %>%
-  mutate(n_contig = n_contiguous)
+for (i in 1:nrow(filtered_zoning)) {
+  neighbors <- neighborhood(g, order = 1, nodes = i)[[1]]
+  # Exclude the node itself from its list of neighbors
+  neighbors <- neighbors[neighbors != i]
+  n_contiguous[i] <- length(neighbors)
+  sum_contig_area[i] <- sum(filtered_zoning$Shape__Area[neighbors], na.rm = TRUE)
+}
 
+contig_info <- data.frame(n_contig = unlist(n_contiguous), sum_contig_area = unlist(sum_contig_area))
+filtered_zoning <- cbind(filtered_zoning, contig_info)
+
+# join council dists----------------------------------------------
+filtered_zoning <- st_join(filtered_zoning, council_dists) %>% 
+                      clean_names()
 
 # save data---------------------------------------------------
-st_write(filtered_zoning, "./data/model_outputs/app_data.geojson")
+st_write(filtered_zoning, "./shiny/app_data.geojson")
